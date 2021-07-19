262db244a73107404f38f306ef12a5d620b43c5e
programming6131 <kimutainashon@gmail.com>
2021-07-19 13:37:40 +0300

    add user

--- lib/screens/adminpage.dart
+++ lib/screens/adminpage.dart
@@ -1,8 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:gmap/screens/add_user.dart';
 
 class AdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Admin"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(30),
+        child: GridView.count(
+          crossAxisCount: 2,
+          children: [
+            Card(
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => AddUser()));
+                },
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.add,
+                        size: 70.0,
+                        color: Colors.lightBlue,
+                      ),
+                      Text(
+                        'Add user',
+                        style: TextStyle(
+                          fontSize: 17.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
